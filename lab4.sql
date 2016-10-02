5cfa442257a5793fa6a41a2cdc4220f9ad9d7ef6
Nicholas D Bradford <Raulwicke@users.noreply.github.com>
2016-10-02 09:07:54 -0400

    Update Lab4
    - Learning from my mistakes: Upload early and often.
    - Bugs Fixes

--- lab4.sql
+++ lab4.sql
@@ -1,15 +1,48 @@
 -- Question 1
-SELECT DISTINCT city 
-FROM Agents 
-WHERE aid IN(
-        SELECT aid  
-    	FROM Orders
-        WHERE cid = 'c002');
+SELECT city
+FROM   AGENTS
+WHERE  aid IN
+   (SELECT aid
+    FROM   orders
+    WHERE  cid = 'c006'
+    );
 
 -- Question 2
+SELECT pid
+FROM ORDERS
+WHERE aid IN
+	(SELECT aid
+     FROM ORDERS
+     WHERE cid in
+     	(SELECT cid
+         FROM CUSTOMERS
+         WHERE city = 'Kyoto')
+    )
+ORDER BY pid DESC;
 
 -- Question 3
-
-
+SELECT cid,name
+FROM CUSTOMERS
+WHERE cid NOT IN
+	(SELECT cid
+     FROM ORDERS
+     WHERE aid IN (
+         SELECT aid 
+         FROM AGENTS
+         WHERE aid = 'a03')
+     );
+-- Question 4
+SELECT cid
+FROM CUSTOMERS
+WHERE cid IN(
+    SELECT cid
+    FROM ORDERS
+    WHERE pid IN(
+        SELECT pid
+        FROM PRODUCTS
+        WHERE pid = 'p01'
+				OR
+        	  pid = 'p07')
+    );
 
 
